ffb2f68fe38d741b88619a371078752238194e81
codeamac07 <codeamac07@gmail.com>
2015-09-15 13:30:46 -0500

    the secon method searches for the index of the letters that are repeated in the string

--- linearsearch.rb
+++ linearsearch.rb
@@ -13,6 +13,20 @@ def linear_search(obj, arr)
     nil
   end
 end
-random_numbers = [ 4, 3, 2, 20, 5, 64, 78, 11, 43 ]
-p linear_search(20, random_numbers)
-p linear_search(29, random_numbers)
+#random_numbers = [ 4, 3, 2, 20, 5, 64, 78, 11, 43 ]
+#p linear_search(20, random_numbers)
+#p linear_search(29, random_numbers)
+
+def global_linear_search(letra, arr)
+  i = 0
+  resp = []
+  while i < arr.length
+    if letra == arr[i]
+      resp.push(i)
+    end
+    i += 1
+  end
+  resp
+end
+arr = "entretenerse".split('')
+p global_linear_search("e", arr)
